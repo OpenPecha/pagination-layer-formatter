d36e92c04aa5f1d0789265997591f90ec6cbe4ee
kaldan007 <kaldantenzin@gmail.com>
2022-06-24 12:31:10 +0530

    text2image serializer updated

--- text2image_serializer.py
+++ text2image_serializer.py
@@ -18,8 +18,8 @@ def get_img_link(ann):
     img_link = f"iiif.bdrc.io/bdr:{img_grp_id}::{img_fn}/full/max/0/default.jpg"
     return img_link
 
-def get_text2image_response(pecha_id, pagination_layer):
-    text2image_response = {
+def get_text2image_alignment(pecha_id, pagination_layer):
+    text2image_alignment = {
         'id': pecha_id,
         'type': "image",
         'alignment' : []
@@ -35,8 +35,8 @@ def get_text2image_response(pecha_id, pagination_layer):
             'target_segment': img_link
         }
         alignments.append(cur_alignment)
-    text2image_response['alignment'] = alignments
-    return text2image_response
+    text2image_alignment['alignment'] = alignments
+    return text2image_alignment
 
 
 
@@ -45,9 +45,9 @@ def serialize_pagination_layer_to_json(opf_id, opf_path, json_path):
     base_names = get_base_names(pecha.opf_path)
     for base_name in base_names:
         pagination_layer = pecha.read_layers_file(base_name, LayerEnum.pagination.value)
-        text2image_response = get_text2image_response(opf_id, pagination_layer)
-        text2image_response = json.dumps(text2image_response, ensure_ascii=False)
-        json_path.write_text(text2image_response, encoding='utf-8')
+        text2image_alignment = get_text2image_alignment(opf_id, pagination_layer)
+        text2image_alignment = json.dumps(text2image_alignment, ensure_ascii=False)
+        json_path.write_text(text2image_alignment, encoding='utf-8')
 
 
 if __name__ == "__main__":
